8e2b752885fde26a8ce0a671b80e8972968ba669
Thorvald Molthe Ballestad <thorvald@simula.no>
2023-03-23 15:42:55 +0100

    Add csv read and write with metadata support

--- decipher/processing/pipeline.py
+++ decipher/processing/pipeline.py
@@ -1,15 +1,21 @@
+import logging
+from functools import partial
 from pathlib import Path
 
 import pandas as pd
+import yaml
 from sklearn.pipeline import Pipeline
 
 from decipher.processing.transformers import AgeAdder, BirthdateAdder, CleanData, ToExam
+
+logger = logging.getLogger(__name__)
 
 
 def read_raw_df(
     screening_data_path: Path,
     dtypes: dict | None = None,
     datetime_cols: list | None = None,
+    **read_csv_kwargs,
 ) -> pd.DataFrame:
     """Read raw screening data into DF"""
     if not screening_data_path.exists():
@@ -26,7 +32,42 @@ def read_raw_df(
         dtype=dtypes,
         parse_dates=datetime_cols,
         dayfirst=True,
+        comment="#",
+        **read_csv_kwargs,
     )
+
+
+def read_from_csv(path: Path, df_reader=None) -> tuple[pd.DataFrame, dict]:
+    """Read df from a csv, with header yaml"""
+    if not path.exists():
+        raise ValueError(f"{path} does not exist!")
+    if df_reader is None:
+        df_reader = partial(pd.read_csv, comment="#")
+    header = ""
+    with open(path) as file:
+        while (line := file.readline()).startswith("#"):
+            header += line.strip("#")[1:]
+    print(header, "-------------")
+    with open(path) as file:
+        print(file.read())
+    header_data = yaml.safe_load(header)
+    data = df_reader(path)
+    return data, header_data
+
+
+def write_to_csv(path: Path, df: pd.DataFrame, metadata: dict, **pd_kwargs) -> None:
+    if path.exists():
+        logger.warning(f"{path} exists, overwriting!")
+    metadata_yaml = yaml.dump(metadata).split("\n")
+    with open(path, "w") as file:
+        # Add a '# ' to the beginning of each line
+        file.write(
+            "\n".join(
+                f"# {line}" for line in metadata_yaml if line
+            )  # Conditional to remove empty lines
+        )
+        file.write("\n")
+    df.to_csv(path, mode="a", **pd_kwargs)
 
 
 def exam_pipeline(birthday_file: Path, drop_missing_birthday: bool = False) -> Pipeline:
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,8 +1,17 @@
+import logging
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
-from decipher.processing.pipeline import exam_pipeline, read_raw_df
+from decipher.processing.pipeline import (
+    exam_pipeline,
+    read_from_csv,
+    read_raw_df,
+    write_to_csv,
+)
+
+logger = logging.getLogger(__name__)
 
 test_data_base = Path(__file__).parent / "test_processing_datasets"
 test_data_screening = test_data_base / "test_screening_data.csv"
@@ -22,3 +31,34 @@ def test_read_and_pipeline():
 
     pipeline = exam_pipeline(birthday_file=test_data_dob, drop_missing_birthday=True)
     pipeline.fit_transform(raw)
+
+
+def test_read_from_csv(tmp_path: Path):
+    data_file = tmp_path / "data.csv"
+    with open(data_file, "w") as file:
+        file.write(
+            "# key1: value1\n"
+            "# key2: value2\n"
+            "# list:\n"
+            "#   - one\n"
+            "#   - two\n"
+            "col1,col2\n"
+            "a,b\n"
+        )
+    data, metadata = read_from_csv(data_file)
+    assert metadata == {
+        "key1": "value1",
+        "key2": "value2",
+        "list": ["one", "two"],
+    }
+    assert pd.DataFrame({"col1": ["a"], "col2": ["b"]}).equals(data)
+
+
+def test_write_to_csv(tmp_path: Path):
+    data_file = tmp_path / "data.csv"
+    df = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
+    metadata = {"key": "value", "list": ["foo", "bar"]}
+    write_to_csv(data_file, df=df, metadata=metadata)
+    with open(data_file) as file:
+        logger.debug(f"Current file content: {file.read()}")
+    assert read_from_csv(data_file)[1] == metadata
